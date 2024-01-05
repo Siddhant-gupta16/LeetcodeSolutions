e180a32c6b48090e2434e0395bd61e57dfb12767
siddhant gupta <95760026+Siddhant-gupta16@users.noreply.github.com>
2024-01-06 00:40:13 +0530

    Time: 16 ms (11.68%), Space: 7.9 MB (19.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-//         sort(s.begin(),s.end());
-//         sort(t.begin(),t.end());
-        
-//         int i=0;
-//         int j=0;
-//         while(i<s.length()){
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.length()!=t.length()){
+          return false;
+        }
+        int i=0;
+        int j=0;
+        while(i<s.length()){
               
-//              if(s[i]!=t[j]){
-//                return false;        
-//              }
-//              i++;j++;
-//         }
-        
-//         return true;
-        
-        
-        int hashmap[256]={0};
-        
-        for(int i=0;i<s.length();i++){
-              hashmap[s[i]]++;    
-         }
-         
-        for(int i=0;i<t.length();i++){
-              hashmap[t[i]]--;    
-         }
-        
-        for(int i=0;i<256;i++){
-              if(hashmap[i]!=0){
-                 return false;  
-              }   
-         }
-        
-        
+             if(s[i]!=t[j]){
+               return false;        
+             }
+             i++;j++;
+        }
         
         return true;
+        
+        
+//         int hashmap[256]={0};
+        
+//         for(int i=0;i<s.length();i++){
+//               hashmap[s[i]]++; 
+//             int a = (int)s[i];
+//             cout<<a<<" ";
+//          }
+         
+//         for(int i=0;i<t.length();i++){
+//               hashmap[t[i]]--;    
+//          }
+        
+//         for(int i=0;i<256;i++){
+//               if(hashmap[i]!=0){
+//                  return false;  
+//               }   
+//          }
+        
+        
+        
+//         return true;
     }
 };
